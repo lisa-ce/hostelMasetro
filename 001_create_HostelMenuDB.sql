326cf17e22573c91fb4f3a5808d7ad77611c4303
lisa-ce <lisachikovore@gmail.com>
2026-03-30 21:25:12 +0200

    Update 001_create_HostelMenuDB.sql

--- 001_create_HostelMenuDB.sql
+++ 001_create_HostelMenuDB.sql
@@ -9,3 +9,16 @@ cycle_id INT PRIMARY KEY IDENTITY(1,1),
 cycle_name VARCHAR(50) NOT NULL,
     cycle_order INT NOT NULL UNIQUE
 );
+
+
+--MENU TABLE
+
+CREATE TABLE menu_item (
+    menu_id INT PRIMARY KEY IDENTITY(1,1),
+    cycle_id INT NOT NULL,
+    day_of_week VARCHAR(20) NOT NULL,
+    meal_type VARCHAR(20) NOT NULL,
+    meal_name VARCHAR(255) NOT NULL,
+    side_items VARCHAR(500),
+    FOREIGN KEY (cycle_id) REFERENCES menu_cycle(cycle_id)
+);
