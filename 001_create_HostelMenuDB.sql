972e248da2949a4589d1cd23ca20678c6a37452c
lisa-ce <lisachikovore@gmail.com>
2026-03-30 21:23:27 +0200

    Update 001_create_HostelMenuDB.sql

--- 001_create_HostelMenuDB.sql
+++ 001_create_HostelMenuDB.sql
@@ -2,3 +2,8 @@ CREATE DATABASE HostelMenuDB;
 GO
 USE HostelMenuDB;
 GO
+CREATE TABLE menu_cycle(
+cycle_id INT PRIMARY KEY IDENTITY(1,1),
+cycle_name VARCHAR(50) NOT NULL,
+    cycle_order INT NOT NULL UNIQUE
+);
